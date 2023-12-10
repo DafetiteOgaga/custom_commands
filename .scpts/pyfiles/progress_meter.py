e09d14fb9ebb4e085d6d1d5f6b7304c76cd326ed
DafetiteOgaga <ogagadafetite@gmail.com>
2023-12-10 14:15:33 +0100

    added to improve user experience when setting up auto commit message for README.md files

---
+++ .scpts/pyfiles/progress_meter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys, time
+
+def progress_bar(duration: int, resp: str):
+    total_iterations = 100
+    per_loop = duration/100
+    if resp.lower() == "y":
+        my_str = "Updating"
+    else:
+        my_str = "Reverting"
+    for i in range(total_iterations+1):
+        time.sleep(per_loop)
+        progress = i / total_iterations
+        print("\r{}: %-40s done %d%%".format(my_str) % ('#' * int(40 * progress), int(100 * progress)), end='')
+    print("")
