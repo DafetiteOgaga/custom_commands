3d6841df9715effe46420d8088c2e78856d2382c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-04-11 09:05:45 +0100

    changes: minor performance fix

--- .scpts/pyfiles/pushfile_main_codes.py
+++ .scpts/pyfiles/pushfile_main_codes.py
@@ -252,6 +252,7 @@ def main_enrty():
 
 
 		while True:
+			print()
 			# print('[y] or "Enter" to push these changes to remote')
 			print('[y] (push) update your remote repository with these changes')
 			print('[q] to abort')
